fed72eaae07067b687f5c9cd0e891e5f1aca2f26
Denis Dashkevich <majorprestige99@gmail.com>
2022-07-07 18:43:23 +0300

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryArray = galleryItems
+	.map(({ preview, original, description }) => {
+		return `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+  `;
+	})
+	.join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryArray);
+
+galleryEl.addEventListener("click", e => {
+	e.preventDefault();
+	if (e.target.nodeName !== "IMG") {
+		return;
+	}
+
+	const instance = basicLightbox.create(`
+	<img src="${e.target.dataset.source}">
+	`);
+
+	instance.show();
+
+	galleryEl.addEventListener("keydown", onKeyUp);
+
+	function onKeyUp(e) {
+		if (e.code !== "Escape") {
+			return;
+		}
+		instance.close();
+		galleryEl.removeEventListener("keydown", onKeyUp);
+	}
+});
